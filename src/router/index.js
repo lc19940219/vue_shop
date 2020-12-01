ba2810b4dfa70bcb2b159ee5903d5399f98babf7
lc <644231692@qq.com>
2020-12-01 21:54:06 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -5,35 +5,40 @@ import Search from '../pages/Search/Search.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Login from '../pages/Login/Login.vue'
+import ShopInfo from "../pages/Shop/ShopGoods/ShopInfo";
+import ShopRatings from "../pages/Shop/ShopGoods/ShopRatings";
+import ShopGoods from "../pages/Shop/ShopGoods/ShopGoods";
+import Shop from "../pages/Shop/Shop";
+
 Vue.use(Router)
 const originalPush = Router.prototype.push
 Router.prototype.push = function push(location) {
-	return originalPush.call(this, location).catch(err => err)
+  return originalPush.call(this, location).catch(err => err)
 }
 export default new Router({
   routes: [{
-      path: '/',
-      redirect: '/msite'
-    },
+    path: '/',
+    redirect: '/msite'
+  },
     {
       path: '/msite',
       component: Msite,
       meta: {
-      showFooter: true
+        showFooter: true
       }
     },
     {
       path: '/search',
       component: Search,
       meta: {
-      showFooter: true
+        showFooter: true
       }
     },
     {
       path: '/order',
       component: Order,
       meta: {
-      showFooter: true
+        showFooter: true
       }
     },
     {
@@ -44,8 +49,30 @@ export default new Router({
       path: '/profile',
       component: Profile,
       meta: {
-      showFooter: true
+        showFooter: true
       }
+    },
+    {
+      path: '/shop',
+      component: Shop,
+      children: [
+        {
+          path: '/shop/goods',
+          component: ShopGoods,
+        },
+        {
+          path: '/shop/ratings',
+          component: ShopRatings,
+        },
+        {
+          path: '/shop/info',
+          component: ShopInfo,
+        },
+        {
+          path: '',
+          redirect: "/shop/goods",
+        }
+      ]
     }
   ]
 })
